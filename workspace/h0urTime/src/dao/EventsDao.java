67ed93aace21788b85d9dc9b5f0f06f25e034ec5
artykalamata <atrykalamata@gmail.com>
2019-06-30 18:58:37 +0200

    removed unused imports in java files

--- workspace/h0urTime/src/dao/EventsDao.java
+++ workspace/h0urTime/src/dao/EventsDao.java
@@ -1,11 +1,8 @@
 package dao;
 
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
--- workspace/h0urTime/src/model/Events.java
+++ workspace/h0urTime/src/model/Events.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
